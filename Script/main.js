50c316550c60dcdff643e72773892e2fa7ae816d
MacWhitehead <mackayla.whitehead7485@stu.mtec.edu>
2020-11-05 07:38:53 -0700

    Added player entry js/changed drop downs to forms

--- Script/main.js
+++ Script/main.js
@@ -1,50 +1,66 @@
-//Questions: 
-//**With bootstrap dropdown menus, how do I make it so it adds 
-//the class 'active' to the dropdown item. 
-//**Is the bootstrap dropdown or the html select option 
-//going to be easier to use for this project? 
-//**How do I get the course info to pull from the API? 
-
-
-const zipCode = document.getElementById('zip-entry');
-const professional = document.getElementById('professional');
-const moderate = document.getElementById('moderate');
-const amateur = document.getElementById('amateur');
-const full18 = document.getElementById('full-18');
-const front9 = document.getElementById('front-9');
-const back9 = document.getElementById('back-9');
-const singlePlayer = document.getElementById('single-player');
-const twoPlayers = document.getElementById('two-players');
-const threePlayers = document.getElementById('three-players');
-const fourPlayers = document.getElementById('four-players');
 const playerEntryContainer = document.getElementById('player-entry');
-const playerDropdown = document.getElementById('player-dropdown');
+const playerDropdown = document.getElementById('player-container');
+const playerCount = document.getElementById('player-count');
 
-let request = new XMLHttpRequest();
-request.open('GET', 'https://golf-courses-api.herokuapp.com/courses', true);
-request.responseType = 'text';
 
-request.send();
-console.log(courses);
-
-function playerDropMenu() {
-    playerDropdown.addEventListener('click');
-    const header = `<h4>Enter the player names: </h4>`;
-    if(click === singlePlayer) {
-        playerDropdown.classList('active');
-        playerEntryContainer.innerHTML(header + createPlayerEntry());
-        console.log("singleplayer clicked");
-    }
+function dropdownCourse() {
+    const courseTemplate = `
+    <a class="dropdown-item" href="#">${courseName}</a>`
 }
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+        console.log(xhttp.responseText);
+        const courseList = JSON.parse(xhttp.responseText);
+        console.log(courseList);
+    }
+};
+xhttp.open("GET", "https://golf-courses-api.herokuapp.com/courses", true);
+xhttp.send();
+
+
+playerDropdown.addEventListener('change', event => {
+    let totalPlayers = playerCount.value;
+    const header = `<h4 class='d-flex flex-column align-items-center' aria-describedby="playerNameHeader">
+    Enter the player names: 
+    </h4>
+    <small id="playerNameHeader" class="text-muted d-flex flex-column align-items-center">
+    Names entered cannot match other players
+  </small>
+    `
+    if (totalPlayers == 1) {
+        playerEntryContainer.innerHTML = (header + createPlayerEntry());
+    }
+    if (totalPlayers == 2) {
+        playerEntryContainer.innerHTML = (header + createPlayerEntry() + createPlayerEntry());
+    }
+    if (totalPlayers == 3) {
+        playerEntryContainer.innerHTML = (header + createPlayerEntry() + createPlayerEntry() + createPlayerEntry());
+    }
+    if (totalPlayers == 4) {
+        playerEntryContainer.innerHTML = (header + createPlayerEntry() + createPlayerEntry() + createPlayerEntry() + createPlayerEntry());
+    }
+});
+
+
 
 function createPlayerEntry() {
     const playerTemplate = `
-<div>
-    <div>
-        Player:
-    </div>
-    <div>
-        <input type="text" placeholder="Name">
-    </div>
-</div> `
+    <div class='container'>
+        <div class='col'>
+            <p class='col-sm'> Player: <p>
+            <input class='col-sm' type="text" placeholder="Name">
+        </div>
+    </div> `
+    return playerTemplate
 }
+
+
+//fix player template so that it is in one line and centered
+//Need to add function that pulls the data selected in the dropdowns
+//Also need a function that verifies that all data has been entered:
+//All options selected, names entered, no duplicate names
+//If duplicate name, module stating names cannot be the same. 
+//Data validation? 
+//Submit button greyed out until all data entered? 
