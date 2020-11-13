7cb1b715653f879b9d87f7b0e7d28993498a0ccf
MacWhitehead <mackayla.whitehead7485@stu.mtec.edu>
2020-11-12 21:33:39 -0700

    Added state mapper + reusable fnct for form divs

--- Script/main.js
+++ Script/main.js
@@ -1,37 +1,40 @@
-const playerEntryContainer = document.getElementById('player-entry');
-const playerDropdown = document.getElementById('player-container');
-const playerCount = document.getElementById('player-count');
-const golfCourseSelector = document.getElementById('course-options');
-const teeTypeSelector = document.getElementById('tee-type');
-const holeTotal = document.getElementById('hole-total-selection');
+import { state } from "./state.js"
+
 const allForms = document.getElementsByClassName('form-control');
 const submitButton = document.getElementById('submit');
 const scorecardDiv = document.getElementById('scorecard');
+const appContainer = document.getElementById('appContainer');
 // const names = document.getElementsByClassName('names');
 
 
-function optionsLogger() {
+function eventListeners() {
+    const golfCourseSelector = document.getElementById('course-options');
+    const teeTypeSelector = document.getElementById('tee-type');
+    const playerCount = document.getElementById('player-count');
+    const holeTotal = document.getElementById('hole-total-selection');
     golfCourseSelector.addEventListener('change', event => {
-        const courseSelected = golfCourseSelector.value;
-        // const courseId = courseList.id;
-        // console.log(courseId);
-        console.log(`selected ${courseSelected}`)
+        state.golfCourse = event.target.value;
+        optionsLogger(event.target.value);
+        populateForms(event.target.value);
     })
     teeTypeSelector.addEventListener('change', event => {
-        let teeSelected = teeTypeSelector.value;
-        console.log(`selected ${teeSelected}`)
+        state.selectedTeeType = event.target.value;
+        optionsLogger(event.target.value)
     })
     holeTotal.addEventListener('change', event => {
-        let numberOfHoles = holeTotal.value;
-        console.log(`selected ${numberOfHoles}`)
+        state.holeTotals = event.target.value;
+        optionsLogger(event.target.value);
     })
     playerCount.addEventListener('change', event => {
-        let numberOfPlayers = playerCount.value;
-        console.log(`selected ${numberOfPlayers}`)
+        state.players = event.target.value;
+        optionsLogger(event.target.value);
     })
 }
 
-optionsLogger()
+function optionsLogger(data) {
+    console.log(`selected ${data}`);
+    console.log(state);
+}
 
 function allFieldsEntered() {
     submitButton.addEventListener('click', event => {
@@ -47,61 +50,97 @@ function allFieldsEntered() {
 allFieldsEntered();
 
 
-function validateNames() {
-    console.log(names);
-    let namesArray = []
-    for(let i = 0; i < names.length; i++) {
-        console.log(names[i].value);
-        if(names[i].value == names[i+1].value) {
-            names[i].classList.add('is-invalid');
-        } else {
-            names[i].classList.remove('is-invalid');
-        }
-    }
-    console.log(names.length);
-}
-
-
 const xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function () {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
         console.log(xhttp.responseText);
         const courseList = JSON.parse(xhttp.responseText);
         console.log(courseList);
+        state.courses = courseList.courses;
+        renderForm('playerTotal', 'player-count', 'number of players');
+        renderForm('holeTotal', 'hole-total-selection', 'number of holes');
+        renderForm('teeTypes', 'tee-type', 'tee type');
+        renderForm('courses', 'course-options', 'course');
+        eventListeners()
+        addPlayer();
     }
 };
 xhttp.open("GET", "https://golf-courses-api.herokuapp.com/courses", true);
 xhttp.send();
 
+function populateForms(value) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            const course = JSON.parse(xhttp.responseText)
+            state.selectedCourse = course;
+            console.log(course)
+        }
+    };
+    xhttp.open("GET", `https://golf-courses-api.herokuapp.com/courses/${value}`, true);
+    xhttp.send();
+};
 
-
-playerDropdown.addEventListener('change', event => {
-    let totalPlayers = playerCount.value;
-    const header = `<h4 class='d-flex flex-column align-items-center' aria-describedby="playerNameHeader">
-    Enter the player names: 
-    </h4>
-    <small id="playerNameHeader" class="text-muted d-flex flex-column align-items-center">
-    Names entered cannot match other players
-  </small>
+function renderForm(stateKey, elementId, placeholder) {
+    //render forms based off state.courses 
+    const courseWrapper = document.createElement('div');
+    const courseDiv = `
+    <div class="form-group">
+    <select class="form-control" id="${elementId}">
+    <option>Please select ${placeholder}</option >
+    ${renderFormOptions(stateKey)}
+    </select >
+    </div >
     `
-    if (totalPlayers == 1) {
-        playerEntryContainer.innerHTML = (header + createPlayerEntry());
-    }
-    if (totalPlayers == 2) {
-        playerEntryContainer.innerHTML = (header + createPlayerEntry() + createPlayerEntry());
-    }
-    if (totalPlayers == 3) {
-        playerEntryContainer.innerHTML = (header + createPlayerEntry() + createPlayerEntry() + createPlayerEntry());
-    }
-    if (totalPlayers == 4) {
-        playerEntryContainer.innerHTML = (header + createPlayerEntry() + createPlayerEntry() + createPlayerEntry() + createPlayerEntry());
-    }
-    namesValidation();
-});
+    appContainer.prepend(courseWrapper);
+    courseWrapper.innerHTML = courseDiv;
+    console.log(courseDiv);
+}
+
+function renderFormOptions(stateKey) {
+    const options = [];
+    state[stateKey].forEach(value => {
+        const option = `
+    <option value="${value.id}"> ${value.name}</option>
+        `
+        options.push(option);
+    });
+    console.log(state[stateKey], options)
+    return options.join('');
+}
+
+function addPlayer() {
+    const playerDropdown = document.getElementById('player-container');
+    const playerEntryContainer = document.getElementById('player-entry');
+    const playerCount = document.getElementById('player-count');
+    playerCount.addEventListener('change', event => {
+        let totalPlayers = playerCount.value;
+        const header = `< h4 class='d-flex flex-column align-items-center' aria - describedby="playerNameHeader" >
+    Enter the player names: 
+    </h4 >
+    <small id="playerNameHeader" class="text-muted d-flex flex-column align-items-center">
+        Names entered cannot match other players
+  </small>
+`
+        if (totalPlayers == 1) {
+            playerEntryContainer.innerHTML = (header + createPlayerEntry());
+        }
+        if (totalPlayers == 2) {
+            playerEntryContainer.innerHTML = (header + createPlayerEntry() + createPlayerEntry());
+        }
+        if (totalPlayers == 3) {
+            playerEntryContainer.innerHTML = (header + createPlayerEntry() + createPlayerEntry() + createPlayerEntry());
+        }
+        if (totalPlayers == 4) {
+            playerEntryContainer.innerHTML = (header + createPlayerEntry() + createPlayerEntry() + createPlayerEntry() + createPlayerEntry());
+        }
+        namesValidation();
+    });
+}
 
 function namesValidation() {
     const names = document.getElementsByClassName('names');
-    for(let i = 0; i < names.length; i++) {
+    for (let i = 0; i < names.length; i++) {
         console.log(names[i].value)
         names[i].addEventListener('onblur', event => {
             console.log('working');
@@ -115,12 +154,28 @@ function namesValidation() {
 };
 
 
+
+function validateNames() {
+    console.log(names);
+    let namesArray = []
+    for (let i = 0; i < names.length; i++) {
+        console.log(names[i].value);
+        if (names[i].value == names[i + 1].value) {
+            names[i].classList.add('is-invalid');
+        } else {
+            names[i].classList.remove('is-invalid');
+        }
+    }
+    console.log(names.length);
+}
+
+
 function createPlayerEntry() {
     const playerTemplate = `
-    <div class='container d-flex justify-content-center'>
+    < div class='container d-flex justify-content-center' >
         <div class='row'>
             <p class='col-3'> Player: <p>
-            <input class='col-md names' type="text" placeholder="Name">
+                <input class='col-md names' type="text" placeholder="Name">
         </div>
     </div> `
     return playerTemplate
